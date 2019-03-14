2a829ddc92d9a35765f8d80acfcbbeac170de686
Zoilo Granda <zgranda@gmail.com>
2019-03-14 17:18:53 -0400

    Ahora se usa directo desde la consola

--- kml_reader/app.js
+++ kml_reader/app.js
@@ -2,15 +2,72 @@ const express = require('express');
 const app = express()
 const http = require('http').Server(app)
 const port = 8080;
+const fs = require('fs');
+const readline = require('readline');
+const xmlparser = require('fast-xml-parser');
 
-app.use(express.static(__dirname + '/public'));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-app.get('/',function (req, res) {
+function askUrl(){
+  let consoleQuestion = 'Inserta el path absoluto hacia el archivo kml/xml" :\n'
+  rl.question(consoleQuestion, (userInput) => {
+    xmlPath = userInput;
+    rl.close();
+    startProcess(xmlPath)
+  });
+};
+
+function startProcess(xmlPath) {
+  fs.readFile(xmlPath,'utf8', function(err, data) {
+    if (!err) {
+      // console.log(data);
+      var jsonObj = xmlparser.parse(data);
+      // console.log(jsonObj);
+      getPlacemarks(jsonObj)
+    }else {
+      console.log('Error leyendo el archivo');
+      console.log(err);
+    }
+  });
+}
+
+function getPlacemarks(jsonObj){
+  let fileContent = jsonObj.kml.Document.Folder;
+  // console.log(fileContent);
+  let placemarksNames = [];
+  for (var place in fileContent) {
+    if (fileContent.hasOwnProperty(place)) {
+      if (fileContent[place].Placemark[1]) {
+        for (var nestedPlace in fileContent[place].Placemark) {
+          if (fileContent[place].Placemark.hasOwnProperty(nestedPlace)) {
+            // console.log(fileContent[place].Placemark[nestedPlace].name);
+            placemarksNames.push(fileContent[place].Placemark[nestedPlace].name)
+          }
+        }
+      }else {
+        // console.log(fileContent[place].Placemark.name);  
+        placemarksNames.push(fileContent[place].Placemark.name)
+      }
+    }
+  }
+  console.log(placemarksNames);
+  generateGG_OVERLAY_POLY(placemarksNames)
+}
+
+function generateGG_OVERLAY_POLY(placemarksNames) {
+  var formatedPlacemarks = placemarksNames.map(function(current) {
+    return `array("${current}")`;
+  });
+  formatedPlacemarks[0] = `array(${formatedPlacemarks[0]}`;
+  formatedPlacemarks[formatedPlacemarks.length-1] +=');';
+  console.log(formatedPlacemarks.toString());
+}
   
-  res.sendFile(__dirname+'/index.html')
-})
-
 http.listen(port,function (err) {
   if (err) return console.log(err);
   console.log('server corriendo ' + port);
+  askUrl()
 })
--- kml_reader/readme.md
+++ kml_reader/readme.md
@@ -1,6 +1,5 @@
 ## Uso
 1. `npm install`
 2. `npm start`
-3. Agregar el archivo kml en la carpeta `public`
-4. Abrir http://localhost:8080/ en el navegador
-5. Colocar el nombre del archivo kml sin extension
+3. Agregar el `/path/absoluto/al/archivo.[xml,kml]`
+4. Copiar el string final y pegarlo en el `$CONFIG["GG_OVERLAY_POLY"]`
